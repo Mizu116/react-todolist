034f1ed190559c59cf416f93363a0e094e76feb3
Mizu <mija.1521@gmail.com>
2022-12-11 16:42:37 +0100

    First TodoList with simple features.

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { useState, useEffect } from "react";
+import "./styles.css";
+
+export default function App() {
+    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useState("");
+    const [activeEdit,setActiveEdit] = useState(false);
+    const [editTodo,setEditTodo] = useState('');
+    const [activeIndex,setActiveIndex] = useState(0);
+
+    function handleInputChange(e) {
+      setTodo(e.target.value);
+    }
+
+    function handleFormSubmit(e) {
+      e.preventDefault();
+      if(todo !== '') {
+        setTodos([...todos,{text: todo, status: "incomplete"}])
+        setTodo('')
+      }
+    }
+
+    function handleEditSubmit(e){
+      e.preventDefault();
+      if(editTodo !== '') {
+        let newTodos = [...todos];
+        newTodos[activeIndex] = {text: editTodo, status: newTodos[activeIndex].status};
+        setTodos(newTodos)
+        setActiveEdit(false)
+      }
+    }
+
+    function handleEditInputChange(e) {
+      setEditTodo(e.target.value);
+    }
+
+    function changeStatus(e, index) {
+      let newTodos = [...todos];
+      newTodos[index] = {text: newTodos[index].text, status: e.target.value};
+      setTodos(newTodos)
+    }
+
+    function handleEdit (todo,index) {
+      setEditTodo(todo)
+      setActiveEdit(true)
+      setActiveIndex(index)
+    }
+
+
+    function handleDelete (index) {
+      setTodos(todos.filter((_todo,indexTodos) => {
+        return indexTodos !== index
+      }))
+    }
+
+    useEffect(() => {
+      console.log(todos)
+    }, [todos])
+    
+  
+    return (
+    <div className="App">
+        <h1>Todo List</h1>
+
+        {!activeEdit &&
+        <form onSubmit={handleFormSubmit}>
+            <input name='todo' type='text' placeholder='Todoを入力' value={todo} onChange={handleInputChange} style={{margin: "2px"}} />
+            <input name='submit' type='submit' value='追加' />
+        </form>
+        }
+        {activeEdit &&
+        <form onSubmit={handleEditSubmit}>
+        <input name='editTodo' type='text' value={editTodo} onChange={handleEditInputChange} />
+        <input name='submitEdit' type='submit' value='更新' />
+        </form>
+        }
+
+    <ul className='todo-list'>
+        {todos.map((todo,index) => (
+            <li><span className='todo-text'>{todo.text}</span>
+            <select name="status" value={todo.status} onChange={(event) => changeStatus(event,index)}>
+              <option value="incomplete">未着手</option>
+              <option value="now">着手</option>
+              <option value="completed">完了</option>
+            </select>
+            <button onClick={() => handleEdit(todo.text,index)}>編集</button>
+            <button onClick={() => handleDelete(index)}>削除</button>
+            </li>
+        ))}
+    </ul>
+    </div>
+  );
+}
